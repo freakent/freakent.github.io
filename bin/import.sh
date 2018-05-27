7cdd0752c2a37ff682819b73f71ee85e2b7118ef
Martin Jarvis <freakent@gmail.com>
2018-05-27 10:41:39 +0100

    Added import script

---
+++ bin/import.sh
@@ -0,0 +1,19 @@
+ruby -rubygems -e 'require "jekyll-import";
+    JekyllImport::Importers::WordPress.run({
+      "dbname"   => "wordpress_db",
+      "user"     => "root",
+      "password" => "W1ndsurf!DO",
+      "host"     => "www.freakent.co.uk",
+      "port"     => "3306",
+      "socket"   => "",
+      "table_prefix"   => "wpa",
+      "site_prefix"    => "",
+      "clean_entities" => true,
+      "comments"       => true,
+      "categories"     => true,
+      "tags"           => true,
+      "more_excerpt"   => true,
+      "more_anchor"    => true,
+      "extension"      => "html",
+      "status"         => ["publish", "draft", "private", "revision"]
+    })'
